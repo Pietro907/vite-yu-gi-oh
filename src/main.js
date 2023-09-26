4b445f33c4499023ee34192cfc7b246a65424a8e
Pietro907 <pietrop.puri@gmail.com>
2023-09-26 17:40:50 +0200

    add page with two component and

--- src/main.js
+++ src/main.js
@@ -12,14 +12,14 @@ Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necess
 il layout come da screenshot allegato.
 
 Al caricamento della pagina, effettuate una chiama ajax all'API di Yu Gi Oh 
-(per chi non li conosce, é na cosa tipo pockemon :faccia_con_mano_sopra_la_bocca: :deluso:
+
 
 https://db.ygoprodeck.com/api/v7/cardinfo.php e con i dati restituiti, stampate una card per ogni carta.
 
 ATTENZIONE:
-l’api restituisce tutti i risultati in un colpo solo :gatto_che_urla:.
+l’api restituisce tutti i risultati in un colpo solo.
 
-:uomo_anziano: Per evitare attese e/o rallentamenti nelle richieste, 
+Per evitare attese e/o rallentamenti nelle richieste, 
 potete diminuire il numero di risultati sfruttando i parametri num e offset
 
 https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0 
